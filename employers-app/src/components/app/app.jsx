34709656790b2e613121d034f7cf83fb18030899
TentaJs <tentait2@gmail.com>
2023-04-25 00:24:28 +0300

    add keys to listItems

--- employers-app/src/components/app/app.jsx
+++ employers-app/src/components/app/app.jsx
@@ -7,10 +7,9 @@ import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 function App(){
     const data = [
-        {name: 'John C.', salary: 800, increase: false},
-        {name: 'Alex M.', salary: 3000, increase: true},
-        {name: 'Carl W.', salary: 5000, increase: false},
-        {name: 'Sarah S.', salary: 15000, increase: false}
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
     ];
     return (
         <div className={'app'}>
