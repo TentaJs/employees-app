34715d0f34c77c5497371f5858ef0bb2de1d73c3
TentaJs <tentait2@gmail.com>
2023-04-27 20:02:04 +0300

    add filter and search function

--- employers-app/src/components/app-filter/app-filter.jsx
+++ employers-app/src/components/app-filter/app-filter.jsx
@@ -1,25 +1,43 @@
-
+import {Component} from "react";
 
 
 import "./app-filter.css";
 
-const AppFilter = () => {
-    return (
-        <div className="btn-group">
-            <button type="button"
-                    className="btn btn-light">
-                All employees
-            </button>
-            <button type="button"
-                    className="btn btn-outline-light">
-                For promotion
-            </button>
-            <button type="button"
-                    className="btn btn-outline-light">
-                Salary over $1,000
-            </button>
-        </div>
-    )
+class AppFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterBtn: ""
+        }
+    }
+
+    filterItems = (e) =>{
+        const filterBtn = e.target.textContent;
+        this.setState({filterBtn})
+        this.props.onUpdateFilter(filterBtn)
+    }
+
+    render() {
+        return (
+            <div className="btn-group">
+                <button type="button"
+                        className="btn btn-light"
+                        onClick={this.filterItems}>
+                    All employees
+                </button>
+                <button type="button"
+                        className="btn btn-outline-light"
+                        onClick={this.filterItems}>
+                    For promotion
+                </button>
+                <button type="button"
+                        className="btn btn-outline-light"
+                        onClick={this.filterItems}>
+                    Salary over $1,000
+                </button>
+            </div>
+        )
+    }
 }
 
 export default AppFilter;
--- employers-app/src/components/app/app.jsx
+++ employers-app/src/components/app/app.jsx
@@ -11,73 +11,91 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data : [
-                {name: 'John C.', salary: 800, increase : false, rise : false, id: 1},
-                {name: 'Alex M.', salary: 3000, increase : false, rise : false, id: 2},
-                {name: 'Carl W.', salary: 5000, increase : false, rise : false, id: 3}
-            ]
+            data: [
+                {name: 'John C.', salary: 800, increase: false, rise: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: false, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
+            ],
+            term: "",
+            filterBtn : ""
         }
         this.maxId = 4;
     }
-    deleteItem = (id) =>{
-        this.setState(({data}) =>{
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
             return {
-                data : data.filter(elem => elem.id !== id)
+                data: data.filter(elem => elem.id !== id)
             }
         })
     }
     addItem = (name, salary) => {
-            const newItem = {
-                name,
-                salary,
-                rise :false,
-                increase: false,
-                id: this.maxId++
+        const newItem = {
+            name,
+            salary,
+            rise: false,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
             }
-            this.setState(({data}) => {
-                const newArr = [...data, newItem];
-                return {
-                    data: newArr
-                }
-            });
+        });
     }
-    onToogleProp = (id, prop) =>{
+    onToogleProp = (id, prop) => {
         this.setState(({data}) => ({
-            data: data.map(item =>{
-                if (item.id === id){
-                    return {...item, [prop] : !item[prop]}
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
                 }
                 return item
             })
         }))
     }
+    onUpdateSearch = (term) =>{
+        this.setState({term})
+    }
 
-    // onToogleRise = (id) =>{
-    //     this.setState(({data}) =>{
-    //         const index = data.findIndex(elem => elem.id === id)
-    //         const old = data[index]
-    //         const newItem = {...old, rise: !old.rise}
-    //         const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)]
-    //         return {
-    //             data : newArr
-    //         }
-    //     })
-    // }
+    searchEmp = (items, term) =>{
+        if (term.length === 0){
+            return items
+        }
+        return items.filter(item =>{
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateFilter = (filterBtn) =>{
+        this.setState({filterBtn})
+    }
+    filterItems = (items, filterBtn) =>{
+        if (filterBtn === 'For promotion'){
+            return items.filter(item => item.rise)
+        }
+        if (filterBtn === "Salary over $1,000"){
+            return items.filter(item => item.salary >= 1000)
+        }
+        return items
+    }
 
     render() {
+        const {data, term, filterBtn} = this.state
         const employees = this.state.data.length
         const increased = this.state.data.filter(item => item.increase).length
+        const visibleData = this.filterItems(this.searchEmp(data,term),filterBtn)
         return (
             <div className={'app'}>
-                <AppInfo employees = {employees} increased = {increased}/>
+                <AppInfo employees={employees} increased={increased}/>
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch = {this.onUpdateSearch}/>
+                    <AppFilter onUpdateFilter = {this.onUpdateFilter}/>
                 </div>
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
-                    onToogleProp = {this.onToogleProp}
+                    onToogleProp={this.onToogleProp}
                 />
                 <EmployeesAddForm onAdd={this.addItem}
                 />
