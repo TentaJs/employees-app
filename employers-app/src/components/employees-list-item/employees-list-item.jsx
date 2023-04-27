85a155fd3da4ea8a7438b5e86e65e11cd9ab1ab5
TentaJs <tentait2@gmail.com>
2023-04-27 17:51:37 +0300

    improvement list function

--- employers-app/src/components/employees-list-item/employees-list-item.jsx
+++ employers-app/src/components/employees-list-item/employees-list-item.jsx
@@ -1,59 +1,38 @@
-import {Component} from "react";
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            favorites: false
-        }
-    }
+const EmployeesListItem = (props) => {
 
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-    onFavorites = () => {
-        this.setState(({favorites}) => ({
-            favorites: !favorites
-        }))
-    }
+    const {name, salary, onDelete, onToogleProp, increase, rise} = props
 
-    render() {
-        const {name, salary, onDelete} = this.props
-        const {increase, favorites} = this.state
-
-        let classNames = "list-group-item d-flex justify-content-between"
-        if (increase === true) {
-            classNames = "list-group-item d-flex justify-content-between increase"
-        }
-        if (favorites === true) {
-            classNames += " like"
-        }
-        return (
-            <li className={classNames}>
-                <span className="list-group-item-label" onClick={this.onFavorites}>{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
-                <div className='d-flex justify-content-center align-items-center'>
-                    <button type="button"
-                            onClick={this.onIncrease}
-                            className="btn-cookie btn-sm ">
-                        <i className="fas fa-cookie"></i>
-                    </button>
-
-                    <button type="button"
-                            className="btn-trash btn-sm "
-                            onClick={onDelete}
-                    >
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        )
+    let classNames = "list-group-item d-flex justify-content-between"
+    if (increase === true) {
+        classNames = "list-group-item d-flex justify-content-between increase"
     }
+    if (rise === true) {
+        classNames += " like"
+    }
+    return (
+        <li className={classNames}>
+            <span className="list-group-item-label" onClick={onToogleProp} data-toogle={'rise'}>{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+            <div className='d-flex justify-content-center align-items-center'>
+                <button type="button"
+                        onClick={onToogleProp}
+                        data-toogle={'increase'}
+                        className="btn-cookie btn-sm ">
+                    <i className="fas fa-cookie"></i>
+                </button>
+
+                <button type="button"
+                        className="btn-trash btn-sm "
+                        onClick={onDelete}
+                >
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
 }
 
 export default EmployeesListItem;
--- employers-app/src/components/employees-list/employees-list.jsx
+++ employers-app/src/components/employees-list/employees-list.jsx
@@ -2,13 +2,14 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data, onDelete}) => {
+const EmployeesList = ({data, onDelete, onToogleProp}) => {
 
     const elements = data.map((item) => {
         const {id, ...itemProps} = item
         return (<EmployeesListItem key={id}
                                    {...itemProps}
                                    onDelete ={() => onDelete(id)}
+                                   onToogleProp ={(e) => onToogleProp(id, e.target.getAttribute('data-toogle') )}
         />)
     })
 
